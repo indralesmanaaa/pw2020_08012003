17a2233439eebdba91afc052c29c9c283439c88e
indralesmana <ilesmana1899@gmail.com>
2022-08-07 02:17:13 +0700

    tadi file ajaxna ke hapus

---
+++ kuliah/pertemuan13/ajax/ajax_cari.php
@@ -0,0 +1,34 @@
+<?php
+
+require '../function.php';
+$mahasiswa = cari($_GET['keyword']);
+
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>Nama</th>
+    <th>Aksi</th>
+  </tr>
+  <?php if (empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p style="color: red; font-style: italic;">Data mahasiswa tidak ditemukan!</p>
+      </td>
+    </tr>
+  <?php endif; ?>
+
+  <?php $i = 1;
+  foreach ($mahasiswa as $mhs) : ?>
+    <tr>
+      <td><?= $i++; ?></td>
+      <td><img src="img/<?= $mhs['gambar']; ?>" width="60"></td>
+      <td><?= $mhs['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $mhs['id']; ?>">Cek Detail</a>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
